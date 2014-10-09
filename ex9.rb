066e6e715d041ad83d057ca381bd3d380cfeaa66
williamkennedy <williamkennedyjnr@gmail.com>
2014-10-09 10:14:07 +0100

    exercise 9 done

--- ex9.rb
+++ ex9.rb
@@ -1,1 +1,17 @@
 #Here's some new strange stuff, remember type it exactly 
+
+days = "Mon Tue Wed Thu Fri Sat Sun" #created a days variable thats assigned to days of week with spaces between each day
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"#months variable with 8 months. It has the "n\" which tells ruby to print it on a new line
+
+puts "Here are the days: #{days}" #prints days to terminal
+puts "Here are the months: #{months}"#prints months to terminal
+
+#The there double-quotes in a row allow you to put as much text in between the next set of treble double-quotes. 
+puts """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
+
+
