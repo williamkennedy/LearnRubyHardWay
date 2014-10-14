565b1fcf895837c2f8c0daec9a0002bccb93d6b0
williamkennedy <williamkennedyjnr@gmail.com>
2014-10-14 22:42:02 +0100

    Finished ex 15

--- ex15.rb
+++ ex15.rb
@@ -1,17 +1,10 @@
 #create a variable called filename which takes a file name as an argument
-filename = ARGV.first
+filename = gets.chomp#changing from ARGV to gets.chomp as per section 5
 
 #create a variable called txt which is equal to the contents of the file
 
 txt = open(filename)
 
-#prints the contents of the file
-puts "Here's your file #{filename}:"
-print txt.read
-
-#creates a variable that prompts the user for the file name
-
-print "Type the file again: "
 file_again = $stdin.gets.chomp
 
 #create a variable called txt which is equal to the contents of the file
@@ -19,5 +12,6 @@ txt_again = open(file_again)
 
 #prints the contents of the txt
 print txt_again.read
-
+txt = close(filename)
+txt_again = close(file_name)
 #on section 4 of this exercise
