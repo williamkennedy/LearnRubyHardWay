f4412701679eb16059e60031fc2221e6a357a79f
williamkennedy <williamkennedyjnr@gmail.com>
2014-10-28 17:31:34 +0000

    started ex33

---
+++ ex33.rb
@@ -0,0 +1,26 @@
+i = 0
+numbers = []
+
+while i < 6
+  puts "At the top i is #{i}"
+  numbers.push(i)
+  
+  i += 1
+  puts "Numbers now: ", numbers
+  puts "At the bottom i is #{i}"
+end
+
+puts "The numbers: "
+
+# remember you can write this other ways
+
+numbers.each {|num| puts num}
+
+numbers.each do |num|
+  puts num
+end
+
+for num in numbers
+  puts num
+end
+
