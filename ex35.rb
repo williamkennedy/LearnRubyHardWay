c858cf9d409fa7995af28b837c8ef7c8a8ce62f1
williamkennedy <williamkennedyjnr@gmail.com>
2014-11-03 23:17:56 +0000

    started ex36

--- ex35.rb
+++ ex35.rb
@@ -1,42 +1,42 @@
-def gold_room
+def gold_room #creates the gold_room
 	puts "This room is full of gold. How much do you take?"
 
 	print "> "
-	choice = $stdin.gets.chomp
+	choice = $stdin.gets.chomp #getting user input
 
-	# this line has a bug, so fix it
-  if choice.include?("0") || choice.include?("1")
-    how_much = choice.to_i
+	# this line has a bug, so fix it #the bug is that user cannot enter digits aside from 0 and 1
+  if choice.include?("0") || choice.include?("1") || choice.include?("2") || choice.include?("3") || choice.include?("4") || choice.include?("5") || choice.include?("6") || choice.include?("7") || choice.include?("8") || choice.include?("9")
+    how_much = choice.to_i #storing user choice as an integer
   else
-    dead("Man, learn to type a number")
+    dead("Man, learn to type a number")#caling the dead function if user does not type a number
   end
   
-  if how_much < 50
+  if how_much < 50#if user number is less than 50
     puts "Nice, you're not greedy, you win!"
-    exit(0)
+    exit(0)#end program
   else
     dead("You greedy bastard!")
   end
 end
 
 
-def bear_room
+def bear_room#define bear room
   puts "There is a bear here."
   puts "The bear has a bunch of honey"
   puts "The fat bear is in front of another door."
   puts "How are you going to move the bear?"
-  bear_moved = false
+  bear_moved = false#set bear moved to false
   
-  while true
+  while true#start an infinte loop until if condition is true
     print '> '
     choice = $stdin.gets.chomp
     
     if choice == 'take honey'
       dead('The bear looks at you then slaps your face off.')
-    elsif choice == "taunt bear" && !bear_moved
+    elsif choice == "taunt bear" && !bear_moved #user enters taunt bear and boolean is set to not false 
       puts "The bear has moved from the door. You can go through it now."
-      bear_moved = true
-    elsif choice == "taunt bear" && bear_moved
+      bear_moved = true #sets boolean to true
+    elsif choice == "taunt bear" && bear_moved 
       dead('The bear gets pissed off and chews your leg')
     elsif choice == "open door" && bear_moved
       gold_room
