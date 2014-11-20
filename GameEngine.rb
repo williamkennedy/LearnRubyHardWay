3078f4a2efeef24ee25419cf121b262de3b4a5e5
williamkennedy <williamkennedyjnr@gmail.com>
2014-11-20 23:11:40 +0000

    started 45

---
+++ GameEngine.rb
@@ -0,0 +1,19 @@
+class Engine 
+
+	def initialize(scene_map)
+		@scene_map = scene_map
+	end
+
+	def play()
+		current_scene = @scene_map.opening_scene()
+		last_scene = @scene_map.next_scene('finished')
+
+		while current_scene != last_scene
+			next_scene_name = current_scene.enter()
+			current_scene = @scene_map.next_scene(next_scene_name)
+		end
+
+		# be sure to print out last scene
+		current_scene.enter()
+	end
+end
