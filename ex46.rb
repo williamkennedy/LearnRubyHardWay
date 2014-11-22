80d20e064a4eeb56b23033e7f12b9ed9d9c525c8
williamkennedy <williamkennedyjnr@gmail.com>
2014-11-22 21:46:45 +0000

    On exercise 46

---
+++ ex46.rb
@@ -0,0 +1,10 @@
+#Read about how to use all of the things you installed.
+#Read about the NAME.gemspec file, commonly called a "Gemspec", and all it has to offer.
+#Make a project put code into the module, then get the module working. This means you have to change all the files, directories, and modules with NAME in them to the name of your project. If you get stuck, watch the video for this exercise to see how I did it.
+#Put a script in the bin directory that you can run. Read about how you can make a Ruby script that's runnable for your system.
+#Mention the bin script you created in your Gemspec so that it gets installed.
+#Use your Gemspec to install your own module and make sure it works, then use gem to uninstall it.
+
+
+#Exercise 1 Checklist: NAME.gemspec, Rakefile, data, ext, tests, bin/Name, doc/, lib/, NAME, NAME.rb, 
+#Gemspec:
