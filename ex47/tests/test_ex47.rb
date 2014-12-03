d8a773d42b3471f0858fd2695d0eb8652f3a6d10
williamkennedy <williamkennedyjnr@gmail.com>
2014-12-03 22:39:39 +0000

    starting ex47

--- ex47/tests/test_ex47.rb
+++ ex47/tests/test_ex47.rb
@@ -1,10 +1,7 @@
-require "./lib/NAME.rb"
+require "ex47/game.rb"
 require "test/unit"
 
-class TestNAME < Test::Unit::TestCase
+class TestGame < Test::Unit::TestCase
 
-	def test_sample
-		assert_equal(4, 2+2)
-	end
-
-end
+	def test_room()
+		gold = Room.new("GoldRoom", )
