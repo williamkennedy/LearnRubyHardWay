772aca445280cdd5a840c8333e6b7047ff9fcaf2
williamkennedy <williamkennedyjnr@gmail.com>
2014-12-10 20:12:32 +0000

    finished exercise 48 but need to refactor it

--- ex48/lib/ex48/lexicon.rb
+++ ex48/lib/ex48/lexicon.rb
@@ -3,8 +3,20 @@ class Lexicon
 		words = words.split(' ')
 		
 		words.collect do |w| 
-			['direction', w]
-		end
-			
+			if w == 'north' || w =='east' || w== 'south'
+			     ['direction', w]
+			elsif w == "go" || w =='kill' || w == 'eat'
+				  ['verb', w]
+			elsif w == "the" || w =='in' || w == 'of'
+				  ['stop', w]
+			elsif w == "bear" || w =='princess'
+				  ['noun', w]
+			elsif w == "ASDFADFASDF" || w =='IAS'
+				  ['error', w]	
+			elsif w.to_i.is_a? Integer
+					w = w.to_i
+			  	  ['number', w]
+			end
+		end			
 	end
 end
